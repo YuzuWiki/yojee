9464d020648951f4a7c1e947eb6d7bb383a1495a
lixianjun <lixianjun@touchdata.io>
2022-01-26 01:20:06 +0800

    [Feat] doc | update table

--- doc/pixiv.sql
+++ doc/pixiv.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS pixiv_user (
     id          BIGINT       AUTO_INCREMENT     PRIMARY KEY,
-    create_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
-    is_delete   BOOL         DEFAULT FALSE       COMMENT '是否删除',
+    created_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    updated_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
+    is_deleted   BOOL         DEFAULT FALSE       COMMENT '是否删除',
 
     uuid        VARCHAR(127) DEFAULT ''         COMMENT 'pixiv uuid',
-    pid         BIGINT       NOT NULL           COMMENT 'user id',
-    name        VARCHAR(256) NOT NULL           COMMENT 'user name',
-    nick_name   VARCHAR(256) DEFAULT ''         COMMENT 'nick name',
-    avatar      VARCHAR(512) DEFAULT ''         COMMENT 'user avatar',
+    pid         BIGINT       NOT NULL           COMMENT 'pixiv user id',
+    name        VARCHAR(256) NOT NULL           COMMENT 'pixiv user name',
+    nick_name   VARCHAR(256) DEFAULT ''         COMMENT 'pixiv nick name',
+    avatar      VARCHAR(512) DEFAULT ''         COMMENT 'pixiv user avatar',
     following   INT          DEFAULT 0          COMMENT '关注数量',
     followers   INT          DEFAULT 0          COMMENT '粉丝数量',
 
@@ -18,9 +18,9 @@ CREATE TABLE IF NOT EXISTS pixiv_user (
 
 CREATE TABLE IF NOT EXISTS pixiv_follow (
     id  BIGINT  AUTO_INCREMENT  PRIMARY KEY ,
-    create_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
-    is_delete   BOOL         DEFAULT FALSE       COMMENT '是否删除',
+    created_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    updated_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
+    is_deleted   BOOL         DEFAULT FALSE       COMMENT '是否删除',
 
     pid             BIGINT       NOT NULL           COMMENT 'user id',
     followed_pid    BIGINT       NOT NULL           COMMENT 'user id',
@@ -29,28 +29,16 @@ CREATE TABLE IF NOT EXISTS pixiv_follow (
     INDEX idx_followed (pid, followed_pid)
 )  CHARACTER SET utf8;
 
-CREATE TABLE IF NOT EXISTS pixiv_tag_group (
-    # 标签组, tage name 为日语
+CREATE TABLE IF NOT EXISTS pixiv_tag(
+    # 日语tag标签(多语言暂不可靠)
     id  BIGINT  AUTO_INCREMENT  PRIMARY KEY ,
-    create_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
-    is_delete   BOOL         DEFAULT FALSE       COMMENT '是否删除',
+    created_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    updated_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
+    is_deleted    BOOL         DEFAULT FALSE       COMMENT '是否删除',
 
-    tag_name        VARBINARY(512)  NOT NULL COMMENT 'tage name',
+    tag_name        VARBINARY(512)  NOT NULL COMMENT 'tage name, jp',
 
     INDEX idx_tag (tag_name)
 )  CHARACTER SET utf8;
 
-CREATE TABLE IF NOT EXISTS pixiv_tag (
-    # 标签组, tage name 为日语
-    id  BIGINT  AUTO_INCREMENT  PRIMARY KEY ,
-    create_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    update_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  '创建时间',
-    is_delete   BOOL         DEFAULT FALSE       COMMENT '是否删除',
 
-    tag_group   BIGINT          NOT NULL COMMENT 'tag group id',
-    language    VARBINARY(16)   NOT NULL COMMENT 'tag标签语种',
-    tag_name    VARBINARY(512)  NOT NULL COMMENT 'tage name',
-
-    INDEX idx_tag_group (tag_group, tag_name)
-)  CHARACTER SET utf8;
--- yojee/model/base.go
+++ yojee/model/base.go
@@ -3,7 +3,8 @@ package model
 import "time"
 
 type BaseMod struct {
-	ID        uint64           	`gorm:"primaryKey,autoIncrement"`
-	CreatedAt *time.Time		`gorm:"autoCreateTime:milli"`
-	UpdatedAt *time.Time		`gorm:"autoUpdateTime:milli"`
+	ID        uint64     `gorm:"primaryKey,autoIncrement"`
+	CreatedAt *time.Time `gorm:"autoCreateTime:milli"`
+	UpdatedAt *time.Time `gorm:"autoUpdateTime:milli"`
+	IsDeleted bool       `gorm:"type:bool;default:false"`
 }
