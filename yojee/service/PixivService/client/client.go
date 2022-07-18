7a0f56b89d8b68d3252edbbbac96a75269f89724
yuzu <like09th@gmail.com>
2022-07-18 18:22:23 +0800

    feat: add client method

--- yojee/service/PixivService/client/client.go
+++ yojee/service/PixivService/client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/cookiejar"
@@ -12,11 +14,40 @@ type (
 	BeforeHook = func(req *http.Request) error
 	AfterHook  = func(resp *http.Response) error
 
+	Query  = url.Values          // request data
+	Params = map[string]struct{} // request body
+
 	HeaderOption struct {
 		Key   string
 		Value string
 	}
 )
+
+// encode url
+func encodeURL(u string, data *Query) (string, error) {
+	URL, err := url.Parse(u)
+	if err != nil {
+		return "", err
+	}
+
+	if data != nil {
+		URL.RawQuery = data.Encode()
+	}
+
+	return URL.String(), nil
+}
+
+func encodeBody(params *Params) (*bytes.Buffer, error) {
+	if params == nil {
+		return nil, nil
+	}
+
+	body, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBuffer(body), nil
+}
 
 type Client struct {
 	http.Client
@@ -69,7 +100,22 @@ func (c *Client) SetCookies(rawURL string, cookies ...*http.Cookie) error {
 	return nil
 }
 
-func (c *Client) Do(req *http.Request) (*http.Response, error) {
+func (c *Client) do(u, method string, query *Query, params *Params) (*http.Response, error) {
+	u, err := encodeURL(u, query)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := encodeBody(params)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, u, body)
+	if err != nil {
+		return nil, err
+	}
+
 	for idx := range c.beforeHooks {
 		if err := c.beforeHooks[idx](req); err != nil {
 			return nil, err
@@ -88,4 +134,20 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 		}
 	}
 	return resp, nil
+}
+
+func (c *Client) Get(u string, query *Query, params *Params) (*http.Response, error) {
+	return c.do(u, http.MethodGet, query, params)
+}
+
+func (c *Client) Post(u string, query *Query, params *Params) (*http.Response, error) {
+	return c.do(u, http.MethodPost, query, params)
+}
+
+func (c *Client) Put(u string, query *Query, params *Params) (*http.Response, error) {
+	return c.do(u, http.MethodPut, query, params)
+}
+
+func (c *Client) Delete(u string, query *Query, params *Params) (*http.Response, error) {
+	return c.do(u, http.MethodDelete, query, params)
 }
