b678a89814c64d9ded6b4d93933feb4fde5ddbf9
yuzu <like09th@gmail.com>
2022-07-21 18:16:18 +0800

    feat: add set/unset proxy

--- yojee/service/PixivService/client/client.go
+++ yojee/service/PixivService/client/client.go
@@ -12,7 +12,25 @@ type (
 type Client struct {
 	http.Client
 
+	// Transport
+	Transport *Transport
+
 	// 钩子函数..
 	BeforeHooks []BeforeHook
 	AfterHooks  []AfterHook
+}
+
+func (c *Client) SetProxy(proxy string) {
+	if c.Transport == nil {
+		c.Transport = NewTransport()
+		c.Client.Transport = c.Transport
+	}
+
+	c.Transport.SetProxy(proxy)
+}
+
+func (c *Client) UnSetProxy() {
+	if c.Transport != nil {
+		c.Transport.UnSetProxy()
+	}
 }
