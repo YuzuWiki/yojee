aad9cdfca61a95c2cd01959077b1c92d0d0d01d7
yuzu <like09th@gmail.com>
2022-07-24 17:22:24 +0800

    feat: add pixiv session

--- yojee/service/PixivService/session.go
+++ yojee/service/PixivService/session.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/like9th/yojee/yojee/common/requests"
+	"net/http"
+	"sync"
 )
 
 var Sessions *Session
 
 type ClientInterface interface {
 	SetProxy(string)
-	UnSetProxy(string)
+	UnSetProxy()
 
 	Get(string, *requests.Query, *requests.Params) (*http.Response, error)
 	Post(string, *requests.Query, *requests.Params) (*http.Response, error)
@@ -18,38 +18,61 @@ type ClientInterface interface {
 	Delete(string, *requests.Query, *requests.Params) (*http.Response, error)
 }
 
-type Session struct {
-	pool map[string]*ClientInterface
+func newClient(phpSessid string) *requests.Client {
+	// new client
+	c := requests.Client{
+		Client:      http.Client{},
+		Transport:   nil,
+		BeforeHooks: []requests.BeforeHook{},
+		AfterHooks:  []requests.AfterHook{},
+	}
+
+	// set default header
+	c.SetHeader(
+		requests.HeaderOption{
+			Key:   "User-Agent",
+			Value: UserAgent,
+		})
+
+	// set cookie
+	if err := c.SetCookies(
+		PixivHost,
+		&http.Cookie{
+			Name:   Phpsessid,
+			Value:  phpSessid,
+			Path:   "/",
+			Domain: PixivDomain,
+		},
+	); err != nil {
+		panic(err.Error())
+	}
+	return &c
 }
 
-func (s *Session) Get(phpSessid string) *ClientInterface {
+type Session struct {
+	pool map[string]ClientInterface
+
+	m sync.Mutex
+}
+
+func (s *Session) Get(phpSessid string) ClientInterface {
 	client, isOk := s.pool[phpSessid]
 	if !isOk {
-		// TODO: new client
+		s.m.Lock()
+		if _, isOk := s.pool[phpSessid]; !isOk {
+			client = newClient(phpSessid)
+			s.pool[phpSessid] = client
+		}
+		s.m.Unlock()
 	}
 	return client
 }
-//
-//func newClient() {
-//	PixivClient := &requests.Client{
-//		Client:      http.Client{},
-//		Transport:   nil,
-//		BeforeHooks: []requests.BeforeHook{},
-//		AfterHooks:  []requests.AfterHook{},
-//	}
-//
-//	PixivClient.SetHeader(
-//		requests.HeaderOption{
-//			Key:   "User-Agent",
-//			Value: UserAgent,
-//		})
-//}
 
-func NewSession() *Session  {
-	return &Session{pool: map[string]*ClientInterface{}}
+func NewSession() *Session {
+	return &Session{pool: map[string]ClientInterface{}}
 }
 
-func init(){
+func init() {
 	if Sessions == nil {
 		Sessions = NewSession()
 	}
