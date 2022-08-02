e716b655ec40acb558cf4a51d2be13ebb5ab126a
yuzu <like09th@gmail.com>
2022-08-02 18:11:45 +0800

    feat: FollowAPI add LastIllusts

--- yojee/service/PixivService/apis/follow_api.go
+++ yojee/service/PixivService/apis/follow_api.go
@@ -1,13 +1,37 @@
 package apis
 
+import (
+	"encoding/json"
+	"net/http"
+
+	pixivService "github.com/like9th/yojee/yojee/service/PixivService"
+)
+
 type FollowAPI struct{}
 
-//func (api FollowAPI) LastIllusts(ctx pixivService.ContextVar, mode string, page int) {
-//	query, err := pixivService.NewQuery(map[string]interface{}{
-//		"p":    page,
-//		"mode": mode,
-//		"lang": "zh",
-//	})
-//
-//	data, err := pixivService.Request(ctx, http.MethodGet, pixivService.Path("/ajax/follow_latest", mode), query, nil)
-//}
+/*
+	LastIllusts return data & error
+		@param
+
+*/
+func (api FollowAPI) LastIllusts(ctx pixivService.ContextVar, mode string, page int) (*FollowLatestDTO, error) {
+	query, err := pixivService.NewQuery(map[string]interface{}{
+		"p":    page,
+		"mode": mode,
+		"lang": "zh",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := pixivService.Request(ctx, http.MethodGet, pixivService.Path("/ajax/follow_latest", mode), query, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body := &FollowLatestDTO{}
+	if err := json.Unmarshal(data, body); err != nil {
+		return nil, err
+	}
+	return body, nil
+}
