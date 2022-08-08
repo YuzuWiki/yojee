935c0fe9cbdd50ee6fcb454bacdc525080ab7bcd
yuzu <like09th@gmail.com>
2022-08-08 18:09:13 +0800

    feat: todo PixivTags

--- service/modelsrv/pixiv_tag.go
+++ service/modelsrv/pixiv_tag.go
@@ -1,1 +1,19 @@
 package modelsrv
+
+type PixivTags struct{}
+
+//func (PixivTags) FindTags(category string, id int64) (*[]model.PixivTagMod, error) {
+//	db := global.DB()
+//
+//	var tags []model.PixivTagMod
+//	switch category {
+//	case apis.Illust:
+//		db = db.Exec()
+//	case apis.Manga:
+//		//
+//	case apis.Novel:
+//		//
+//	default:
+//		return nil, errors.New(fmt.Sprintf("category(%s) not support", category))
+//	}
+//}
